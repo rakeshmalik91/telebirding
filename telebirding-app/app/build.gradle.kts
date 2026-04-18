d680923b1cec1e884a3dc2d8c6b1fd2f6dae025b
Rakesh Malik <rakeshmalik91@gmail.com>
2026-04-18 08:22:41 +0530

    app release v1.0.3

--- telebirding-app/app/build.gradle.kts
+++ telebirding-app/app/build.gradle.kts
@@ -6,16 +6,14 @@ plugins {
 
 android {
     namespace = "com.rakeshmalik.telebirding"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.rakeshmalik.telebirding"
         minSdk = 26
-        targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.2"
+        targetSdk = 35
+        versionCode = 3
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -50,10 +48,10 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation("androidx.compose.material:material")
-    implementation("androidx.compose.material:material-icons-extended")
-    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation(libs.androidx.compose.material)
+    implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.androidx.swiperefreshlayout)
+    implementation(libs.kotlinx.coroutines.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
